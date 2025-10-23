e92e4c542e54ef9cfffaa41fe1f164d3e5981200
Azarya Rozet <azrozet@gmail.com>
2025-10-23 21:48:11 +0300

    "ПРОГРАММА ПО ДНЯМ" в hero subtitle

--- content.js
+++ content.js
@@ -1,7 +1,7 @@
 const CONTENT = {
   hero: {
     title: "Индивидуальный почерк ар-деко<br><span class=\"hero-accent\">100<br>лет</span><br>•",
-    subtitle: "Фактуры, материалы, атмосфера. <br>То, что не&nbsp;видно в&nbsp;публикациях. <br>4&nbsp;дня с&nbsp;кураторами.",
+    subtitle: "Фактуры, материалы, атмосфера. <br>То, что не&nbsp;видно в&nbsp;публикациях. <br>4&nbsp;дня с&nbsp;кураторами. <br>Программа по&nbsp;дням.",
     dates: "15–18+ января 2026",
     group: "до&nbsp;12 человек",
     price: "1 550&nbsp;€"
