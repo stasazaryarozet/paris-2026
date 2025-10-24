4e04c6ecf641d092ba055091825753fdb1cf8734
Azarya Rozet <azrozet@gmail.com>
2025-10-24 18:18:03 +0300

    Правка 57 (v57): буллит-разделитель после 'контакт с Реальностью'

--- content.js
+++ content.js
@@ -14,13 +14,14 @@ const CONTENT = {
     ogDescription: "4&nbsp;дня. Фактуры, материалы, атмосфера. Можно ли&nbsp;ощутить словами?",
     ogImage: "https://parisinjanuary.ru/og-image.jpg",
     url: "https://parisinjanuary.ru",
-    version: "56"
+    version: "57"
   },
   program: {
     intro: [
       "Денье, Легре (Legré), <span class=\"emphasis\">Эйлин Грей</span>, Аалто. У&nbsp;каждого свой <span class=\"emphasis\">почерк</span>.",
       "В&nbsp;интерьерах видны соотношения фактур, тонкости цвета, работа с&nbsp;материалами.",
       "Не&#124;обход&#124;им ли<br><span class=\"emphasis\">контакт с&nbsp;Реальностью</span><br>?",
+      "<div class=\"curator-separator\">•</div>",
       "<h2 class=\"material-title\"><strong>Материал</strong>&nbsp;— главный источник вдохновения</h2>",
       "Парижские дизайнеры смешивают новое со&nbsp;старым. Интерьер без&nbsp;предметов с&nbsp;историей выглядит неживо."
     ],
