e5d7d6ffe0e24311bccc5d4ad67dcd8cbe25932b
Azarya Rozet <azrozet@gmail.com>
2025-10-24 18:19:19 +0300

    Правка 58 (v58): конгруэтный блок 'Материал' — центрирован, увеличен, медный акцент

--- content.js
+++ content.js
@@ -14,7 +14,7 @@ const CONTENT = {
     ogDescription: "4&nbsp;дня. Фактуры, материалы, атмосфера. Можно ли&nbsp;ощутить словами?",
     ogImage: "https://parisinjanuary.ru/og-image.jpg",
     url: "https://parisinjanuary.ru",
-    version: "57"
+    version: "58"
   },
   program: {
     intro: [
