20326c3fec4389c26cb9eef005373c3ff3248ed6
Azarya Rozet <azrozet@gmail.com>
2025-10-24 18:23:01 +0300

    Правка 60 (v60): минималистичный разделитель — тонкая медная линия

--- content.js
+++ content.js
@@ -14,7 +14,7 @@ const CONTENT = {
     ogDescription: "4&nbsp;дня. Фактуры, материалы, атмосфера. Можно ли&nbsp;ощутить словами?",
     ogImage: "https://parisinjanuary.ru/og-image.jpg",
     url: "https://parisinjanuary.ru",
-    version: "59"
+    version: "60"
   },
   program: {
     intro: [
