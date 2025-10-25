7f08c73750f3bd73c5ede186b9e76900b4818db4
Azarya Rozet <azrozet@gmail.com>
2025-10-25 19:16:01 +0300

    Expand route to 1925-1937, use Russian name for Dufy. v103

--- content.js
+++ content.js
@@ -14,7 +14,7 @@ const CONTENT = {
     ogDescription: "4&nbsp;дня. Фактуры, материалы, атмосфера. Можно ли&nbsp;ощутить словами?",
     ogImage: "https://parisinjanuary.ru/og-image.jpg",
     url: "https://parisinjanuary.ru",
-    version: "102"
+    version: "103"
   },
   program: {
     intro: [
@@ -71,7 +71,7 @@ const CONTENT = {
         },
         {
           name: "Palais de Tokyo + Trocadéro",
-          description: "Маршрут выставки Expo 1937. «Фея Электричества» Raoul Dufy&nbsp;— 250 панелей&nbsp;— крупнейшая картина эпохи. «Лебединая песня» стиля"
+          description: "Маршрут 1925–1937. «Фея Электричества» Рауля Дюфи&nbsp;— 250 панелей&nbsp;— крупнейшая картина эпохи. «Лебединая песня» стиля"
         }
       ]
     },
