39159f99f4c96a0e7d4bce37a3184fa51c9546ca
Azarya Rozet <azrozet@gmail.com>
2025-10-25 19:26:23 +0300

    Reformat curators line in inclusions. v110

--- content.js
+++ content.js
@@ -14,7 +14,7 @@ const CONTENT = {
     ogDescription: "4&nbsp;дня. Фактуры, материалы, атмосфера. Можно ли&nbsp;ощутить словами?",
     ogImage: "https://parisinjanuary.ru/og-image.jpg",
     url: "https://parisinjanuary.ru",
-    version: "109"
+    version: "110"
   },
   program: {
     intro: [
@@ -144,8 +144,8 @@ const CONTENT = {
   inclusions: [
     {
       icon: "+",
-      title: "Кураторы 4&nbsp;дня",
-      description: "Материалы, чат"
+      title: "Кураторы:",
+      description: "4&nbsp;дня. Материалы, чат"
     },
     {
       icon: "±",
