a92f36eb70cf08d7d8231bdffd76810cebe42de4
Azarya Rozet <azrozet@gmail.com>
2025-10-25 19:16:55 +0300

    Remove Dufy transliteration, use original name. v104

--- content.js
+++ content.js
@@ -14,7 +14,7 @@ const CONTENT = {
     ogDescription: "4&nbsp;дня. Фактуры, материалы, атмосфера. Можно ли&nbsp;ощутить словами?",
     ogImage: "https://parisinjanuary.ru/og-image.jpg",
     url: "https://parisinjanuary.ru",
-    version: "103"
+    version: "104"
   },
   program: {
     intro: [
@@ -71,7 +71,7 @@ const CONTENT = {
         },
         {
           name: "Palais de Tokyo + Trocadéro",
-          description: "Маршрут 1925–1937. «Фея Электричества» Рауля Дюфи&nbsp;— 250 панелей&nbsp;— крупнейшая картина эпохи. «Лебединая песня» стиля"
+          description: "Маршрут 1925–1937. «Фея Электричества» Raoul Dufy&nbsp;— 250 панелей&nbsp;— крупнейшая картина эпохи. «Лебединая песня» стиля"
         }
       ]
     },
