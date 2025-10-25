7390b36597e08281cbd6284ad4b1437985e12466
Azarya Rozet <azrozet@gmail.com>
2025-10-25 19:20:27 +0300

    Remove line break in Liaigre description. v107

--- content.js
+++ content.js
@@ -14,7 +14,7 @@ const CONTENT = {
     ogDescription: "4&nbsp;дня. Фактуры, материалы, атмосфера. Можно ли&nbsp;ощутить словами?",
     ogImage: "https://parisinjanuary.ru/og-image.jpg",
     url: "https://parisinjanuary.ru",
-    version: "106"
+    version: "107"
   },
   program: {
     intro: [
@@ -47,7 +47,7 @@ const CONTENT = {
         },
         {
           name: "Christian Liaigre",
-          description: "Шлифовка дерева до&nbsp;шелковистости.\nГлухой блеск и отточенные кромки камня. Чистота, точность&nbsp;— тишина формы"
+          description: "Шлифовка дерева до&nbsp;шелковистости. Глухой блеск и отточенные кромки камня. Чистота, точность&nbsp;— тишина формы"
         },
         {
           name: "Музей",
