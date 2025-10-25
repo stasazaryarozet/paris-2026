0d56e3a1f4d30dc109125a72a6b6c310e684bdf8
Azarya Rozet <azrozet@gmail.com>
2025-10-25 19:54:27 +0300

    v116: Expand hero CTA buttons horizontally

--- content.js
+++ content.js
@@ -14,7 +14,7 @@ const CONTENT = {
     ogDescription: "4&nbsp;дня. Фактуры, материалы, атмосфера. Можно ли&nbsp;ощутить словами?",
     ogImage: "https://parisinjanuary.ru/og-image.jpg",
     url: "https://parisinjanuary.ru",
-    version: "115"
+    version: "116"
   },
   program: {
     intro: [
